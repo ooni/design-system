798880d2078be8e50738367d32acb5f32256d8be
Arturo Filastò <arturo@filasto.net>
2018-07-23 14:00:01 +0200

    Geneate react components for our custom icons

---
+++ components/icons/CategoryCodeIGO.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Icon from 'react-icon-base'
+const CategoryCodeIGO = props => (
+  <Icon viewBox="0 0 16 16" {...props}>
+    <g>
+  <title>CategoryCodeIGO</title>
+  <g>
+    <path d="M9.065,5.954a3.42,3.42,0,0,1-.1.828A3.407,3.407,0,0,1,11.3,9.218,3.4,3.4,0,1,0,8,3.482,3.392,3.392,0,0,1,9.065,5.954Z"/>
+    <path d="M4.7,9.218A3.407,3.407,0,0,1,7.036,6.782,3.411,3.411,0,0,1,8,3.482,3.4,3.4,0,1,0,4.7,9.218Z"/>
+    <path d="M8,8.426a3.4,3.4,0,0,1-3.3.792,3.4,3.4,0,1,0,6.6,0A3.4,3.4,0,0,1,8,8.426Z"/>
+  </g>
+</g>
+  </Icon>
+)
+export default CategoryCodeIGO
---
+++ components/icons/NettestGroupMiddleBoxes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Icon from 'react-icon-base'
+const NettestGroupMiddleBoxes = props => (
+  <Icon viewBox="0 0 30 30" {...props}>
+    <g>
+  <defs>
+    
+  </defs>
+  <title>Middle Boxes</title>
+  <g style="(#clip-path)">
+    <polygon points="22.232 6.653 10.382 12.578 10.382 17.022 7.419 15.541 7.419 11.097 19.269 5.172 14.826 2.95 2.976 8.875 2.976 20.725 14.826 26.65 26.676 20.725 26.676 8.875 22.232 6.653"/>
+  </g>
+</g>
+  </Icon>
+)
+export default NettestGroupMiddleBoxes
