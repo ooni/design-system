a526be50b1b6f5bfc212fa7252f9763354a86547
Arturo Filastò <arturo@filasto.net>
2018-07-23 15:32:02 +0200

    Update custom icons

--- components/icons/NettestGroupPerformance.js
+++ components/icons/NettestGroupPerformance.js
@@ -7,7 +7,7 @@ const NettestGroupPerformance = props => (
     
   </defs>
   <title>Performance</title>
-  <g style="(#clip-path)">
+  <g>
     <polygon points="9.61 4.234 21.196 4.234 16.573 13.499 21.196 13.499 13.091 27.387 13.091 16.961 9.61 16.961 9.61 4.234"/>
   </g>
 </g>
