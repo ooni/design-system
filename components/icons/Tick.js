baef0b9f56f01c997e2db1c63fd9fbdc40ea3062
Arturo Filastò <arturo@filasto.net>
2018-07-26 13:51:03 +0200

    Update icons

--- components/icons/Tick.js
+++ components/icons/Tick.js
@@ -3,12 +3,8 @@ import Icon from 'react-icon-base'
 const Tick = props => (
   <Icon viewBox="0 0 30 30" {...props}>
     <g>
-  <defs>
-    
-  </defs>
-  <title>Tick</title>
   <g>
-    <path d="M2.286,14.605l4.138-4.144c1.971,1.974,3.24,3.247,5.218,5.23L22.825,4.508l4.16,4.169L11.666,23.992Z" style="fill: #21262a"/>
+    <path d="M2.286,14.605l4.138-4.144c1.971,1.974,3.24,3.247,5.218,5.23L22.825,4.508l4.16,4.169L11.666,23.992Z"/>
   </g>
 </g>
   </Icon>
