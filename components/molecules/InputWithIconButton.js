4a833165167a55d5a1a5681bbb613ebc3612ace1
Arturo Filastò <arturo@filasto.net>
2018-09-21 16:02:42 +0200

    Lint Input and theme

--- components/molecules/InputWithIconButton.js
+++ components/molecules/InputWithIconButton.js
@@ -3,19 +3,8 @@ import PropTypes from 'prop-types'
 
 import { Flex, Box } from '@rebass/grid'
 
-import styled, { css } from 'styled-components'
-import { space, fontSize, fontSizeMult, color, width } from '../util'
-
 import Input from '../atoms/Input'
 import IconButton from '../atoms/IconButton'
-
-const ButtonContainer = styled.div`
-  ${space}
-
-  color: ${props => props.theme.colors.gray5};
-  margin: 0 auto;
-  height: 16px;
-`
 
 export const InputWithIconButton = (props) => {
   return (
@@ -24,8 +13,8 @@ export const InputWithIconButton = (props) => {
         <Box w={9/10} mt={2}><Input {...props} /></Box>
         <Box onClick={props.onAction} w={1/10}>
           <IconButton pl={3} pr={3}
-              onClick={props.onAction}
-              icon={props.icon} />
+            onClick={props.onAction}
+            icon={props.icon} />
         </Box>
       </Flex>
     </div>
