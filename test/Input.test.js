19f32d8215820224aa027d0966ff94acdee751f1
Sarath M S <sarath.ms@gmail.com>
2018-09-25 18:00:06 -0400

    Update test for Input

--- test/Input.test.js
+++ test/Input.test.js
@@ -1,6 +1,7 @@
-/* global describe, test, expect */
+/* global describe, test, expect, jest */
 import React from 'react'
-import { renderJSON, renderShallowJSON } from './index'
+import toJson from 'enzyme-to-json'
+import { renderJSON, renderShallowJSON, shallowWithTheme, mountWithTheme } from './index'
 import { Input } from '../components'
 
 describe('Input', () => {
@@ -9,5 +10,40 @@ describe('Input', () => {
       <Input error='Some error' />
     )
     expect(json).toMatchSnapshot()
+  })
+
+  test('renders with prop type', () => {
+    const json = renderShallowJSON(
+      <Input type='number' />
+    )
+    expect(json).toMatchSnapshot()
+  })
+
+  test('renders with prop rows', () => {
+    const json = renderShallowJSON(
+      <Input type='textarea' rows={4} />
+    )
+    expect(json).toMatchSnapshot()
+  })
+
+  test('renders with prop fontSize', () => {
+    const json = renderShallowJSON(
+      <Input fontSize={4} />
+    )
+    expect(json).toMatchSnapshot()
+  })
+
+  test('renders with onChange handler', () => {
+    // const value = 'OONI'
+    // const onChange = jest.fn()
+    const wrapper = renderShallowJSON(
+      <Input onChange={jest.fn()} />
+    )
+    expect(wrapper).toMatchSnapshot()
+    // XXX: Should we simulate an event
+    // wrapper.find('input').simulate('change', {
+    //   target: { value }
+    // })
+    // expect(onChange).toBeCalledWith(value)
   })
 })
