32397f8fb7eff0b7c539f9470667023dda7c62e7
Sarath M S <sarath.ms@gmail.com>
2019-12-03 20:59:34 -0500

    Add icon for Circumvention test group

---
+++ components/icons/NettestGroupCircumvention.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Icon from 'react-icon-base'
+
+const NettestGroupCircumvention = props => (
+  <Icon viewBox='0 0 30 30' {...props}>
+    <g>
+      <title>Circumvention</title>
+      <g>
+        <path d="M18.0076825,14.1283019 L14,14.1283019 L14,9.6 L18.0076825,9.6 L18.0076825,6.30943396 L24,11.7433962 L18.0076825,17.1773585 L18.0076825,14.1283019 Z M0,9.6 L4.60947503,9.6 L4.60947503,14.1283019 L0,14.1283019 L0,9.6 Z M7,-2.84217094e-14 L11.609475,-2.84217094e-14 L11.609475,24 L7,24 L7,-2.84217094e-14 Z" id="Triangle-2"></path>
+      </g>
+    </g>
+  </Icon>
+)
+
+export default NettestGroupCircumvention
