6874320ef46b55ee9b19f97c556ac99843f2ae43
Sarath M S <sarath.ms@gmail.com>
2019-12-18 06:02:45 -0500

    Upgrade to styled-components@4.x.x and test utilities
    We now use the '@testing-library/react' library to write tests
    Because this enables a single render of the tested component
    to be used for snapshot testing, style testing and DOM testing
    enzyme and 'react-test-renderer' didn't allow this unless we render
    the component with different renderers and convert to JSON

--- test/index.js
+++ test/index.js
@@ -1,41 +1,21 @@
 import 'jest-styled-components'
 import React from 'react'
-
-import toJson from 'enzyme-to-json'
-import { mount, shallow } from 'enzyme'
-
 import { ThemeProvider } from 'styled-components'
+import { render } from '@testing-library/react'
 
 import {
-  theme,
+  theme
 } from '../components'
 
 import './config.js'
 
-export const mountWithTheme = (el) => {
-  const context = shallow(<ThemeProvider theme={theme} />)
-    .instance()
-    .getChildContext()
-
-  return mount(el, {
-    context,
-    childContextTypes: ThemeProvider.childContextTypes,
+export const renderWithTheme = (el, customTheme) => {
+  const ProviderWrapper = ({ children }) => (
+    <ThemeProvider theme={customTheme || theme}>
+      {children}
+    </ThemeProvider>
+  )
+  return render(el, {
+    wrapper: ProviderWrapper
   })
 }
-
-export const shallowWithTheme = (el) => {
-  // FIXME for v4 the following:
-  // https://github.com/styled-components/jest-styled-components#theming doesn't
-  // work with styled-components v4, because getChildContext() is replaced with
-  // childContext(), but calling that function leads to other odd errors
-  //The following workaround also doesn't work due to:
-  //https://github.com/airbnb/enzyme/issues/1647
-
-  const context = shallow(<ThemeProvider theme={theme} />)
-    .instance()
-    .getChildContext()
-  return shallow(el, { context })
-}
-
-export const renderJSON = el => toJson(mountWithTheme(el))
-export const renderShallowJSON = el => toJson(shallowWithTheme(el))
