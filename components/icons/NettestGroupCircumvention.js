a8010985bdcb9534b3d45cd34905ab4e82c1a62a
Arturo Filastò <arturo@filasto.net>
2020-04-14 11:38:28 +0200

    Fix icon and create-icons

--- components/icons/NettestGroupCircumvention.js
+++ components/icons/NettestGroupCircumvention.js
@@ -3,13 +3,7 @@ import Icon from 'react-icon-base'
 const NettestGroupCircumvention = props => (
   <Icon viewBox="0 0 30 30" {...props}>
     <g>
-  <defs>
-    
-  </defs>
-  <title>Circumvention Test</title>
-  <g>
-    <path d="M18.0076825,14.1283019 L14,14.1283019 L14,9.6 L18.0076825,9.6 L18.0076825,6.30943396 L24,11.7433962 L18.0076825,17.1773585 L18.0076825,14.1283019 Z M0,9.6 L4.60947503,9.6 L4.60947503,14.1283019 L0,14.1283019 L0,9.6 Z M7,-2.84217094e-14 L11.609475,-2.84217094e-14 L11.609475,24 L7,24 L7,-2.84217094e-14 Z" id="Triangle-2"/>
-  </g>
+<path id="circumvention" d="M21,17.1h-4v-4.5h4V9.3l6,5.4l-6,5.4V17.1z M3,12.6h4.6v4.5H3V12.6z M10,3h4.6v24H10V3z"/>
 </g>
   </Icon>
 )
