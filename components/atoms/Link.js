4a54703c5bee2e8ac8259825b512487bb3c45e38
Rick <codingapps>
2020-04-16 19:01:16 -0700

    Updated Link default color to theme.colors.blue6 .
    Updated Link default color to theme.colors.blue6 instead of default
    color to theme.colors.blue5.

--- components/atoms/Link.js
+++ components/atoms/Link.js
@@ -3,7 +3,7 @@ import { Link } from 'rebass'
 import styled from 'styled-components'
 
 const StyledLink = styled(Link)`
-  color: ${props => props.color || props.theme.colors.blue5};
+  color: ${props => props.color || props.theme.colors.blue6};
   text-decoration: none;
 
   &:hover {
