0eb3c89e9d89f1dfc3054342ae3401f18a7d8623
Shivam Shekhar <shivamshekhar.i7@gmail.com>
2021-03-25 19:37:50 +0530

    Change default theme color for Link from blue7 to blue6 (#109)
    * Changed rebass theme link color from blue7 to blue6
    
    * Tests updated to test for updated default color (blue6) in Link component instead of blue7

--- components/theme/rebassTheme.js
+++ components/theme/rebassTheme.js
@@ -103,7 +103,7 @@ export const rebassTheme = {
       maxWidth: [768, 1024, 1280]
     },
     link: {
-      color: 'blue7',
+      color: 'blue6',
       textDecoration: 'none',
       ':hover': {
         filter: 'brightness(125%)'
