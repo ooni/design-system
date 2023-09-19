bb9a75c546bb5543e22f3a85b9ee5317aaba4dbf
majakomel <maja.komel@gmail.com>
2023-09-19 15:54:21 +0200

    Fix radio group

--- src/components/RadioGroup.tsx
+++ src/components/RadioGroup.tsx
@@ -1,12 +1,4 @@
-import React, {
-  useState,
-  ChangeEvent,
-  ReactNode,
-  FC,
-  Children,
-  cloneElement,
-  ReactElement,
-} from 'react'
+import React, { ChangeEvent, ReactNode, Children, cloneElement } from 'react'
 import { FlexProps } from 'types'
 import Flex from './Flex'
 
@@ -17,63 +9,33 @@ export interface IRadioGroup extends FlexProps {
   direction?: 'row' | 'column'
 }
 
-const RadioGroup = (props: IRadioGroup) => {
-  const { children, name, value, direction, onChange, ...rest } = props
+const RadioGroup = ({
+  children,
+  name,
+  value,
+  onChange,
+  direction = 'column',
+  ...props
+}: IRadioGroup) => {
+  const iterateOverChildren = (children: ReactNode) => {
+    return Children.map(children, (child: ReactNode): ReactNode => {
+      if (!React.isValidElement(child)) return null
 
-  const [currentValue, setCurrentValue] = useState(value)
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setCurrentValue(event.target.value)
+      return cloneElement(child, {
+        ...child.props,
+        checked: child.props.value === value,
+        onChange: (e: ChangeEvent<HTMLFormElement>) =>
+          onChange && onChange(e.target.value),
+        children: iterateOverChildren(child.props.children),
+      })
+    })
   }
 
   return (
-    <Flex flexDirection={direction} {...rest} onChange={handleChange}>
-      {Children.map(children, (child) => {
-        const childElement = child as ReactElement
-
-        if (childElement.props.name === name) {
-          return child
-        }
-
-        return cloneElement(childElement, {
-          name,
-          checked: childElement.props.value === currentValue,
-        })
-      })}
+    <Flex flexDirection={direction} {...props}>
+      {iterateOverChildren(children)}
     </Flex>
   )
 }
-
-RadioGroup.defaultProps = {
-  direction: 'column',
-}
-
-// export const RadioGroup = ({
-//   children,
-//   name,
-//   value,
-//   onChange,
-//   direction = 'column',
-//   ...props
-// }) => {
-
-//   const iterateOverChildren = (children) => {
-//     return React.Children.map(children, (child) => {
-//       if (!React.isValidElement(child)) return
-
-//       return React.cloneElement(child, {
-//         ...child.props,
-//         checked: child.props.value === value,
-//         onChange: (e) => { onChange(e.target.value) },
-//         children: iterateOverChildren(child.props.children)})
-//     })
-//   }
-
-//   return (
-//     <Flex flexDirection={direction} {...props}>
-//       {iterateOverChildren(children)}
-//     </Flex>
-//   )
-// }
 
 export default RadioGroup
