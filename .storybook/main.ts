fae14e554b19b43527391ebc46bdbc4ff8adc3c5
majakomel <maja.komel@gmail.com>
2024-04-23 18:29:47 +0200

    Update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,7 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
     '@storybook/addon-mdx-gfm',
+    '@chromatic-com/storybook'
   ],
   framework: {
     name: '@storybook/react-vite',
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -6,7 +6,6 @@ import theme from '../src/theme'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
